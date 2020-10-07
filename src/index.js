496902dffca3a163242c3c757ad52ae8ec3f348e
Sail <865605793@qq.com>
2020-10-07 16:01:45 +0800

    feat: add css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import './index.css'
+
 console.log('================== feedback sdk引入成功 ===================');
 console.log('================== feedback sdk引入成功 1===================');
+
+
+
+const makeDom = () => {
+	const body = document.querySelector('body')
+	const childNode = document.createElement('div');
+	childNode.innerHTML = '<span>〜</span>';
+	childNode.setAttribute('class', '__feedback-wrap');
+	body.appendChild(childNode)
+}
+
+makeDom();
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,30 @@ module.exports = {
 			template: './index.html',
 			inject: true
 		})
-    ]
+	],
+	module: {
+		rules: [
+		  // 对js文件进行babel-loader处理（将ES6语法转换成ES5）
+			{
+			  test: /\.js$/,
+			  exclude: /node_modules/,
+			  loader: "babel-loader",
+			  options: {
+				presets: [
+				  [
+					"@babel/preset-env",// 将ES6语法转成ES5
+					{
+					  // 低版本浏览器中只补充项目中使用到的ES6语法
+					  useBuiltIns: "usage" 
+					}
+				  ]
+				]
+			  }
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			}
+		]
+	  }
 }
